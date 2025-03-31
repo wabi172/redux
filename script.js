384f2be26e6d33dad08f8240b5effbee65eb563a
wabi172 <114135493+wabi172@users.noreply.github.com>
2025-03-31 17:00:04 +0800

    store

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const redux =  require('redux');
+const createStore = redux.createStore;
 const ORDER_PIZZA = 'ORDER_PIZZA'
 
 // Action
@@ -7,17 +9,16 @@ const ORDER_PIZZA = 'ORDER_PIZZA'
 // }
 
 // Action Creator
-function ORDER_PIZZA(){
+function orderPiazza(){
     return {
         type:ORDER_PIZZA,
-        shop_name:'pizza shop'
+        payload:90
     } 
 }
 
 // Reducer 
 const initialState = {
     pizzaBase:100,
-    toppings:['cheese','capsicum']
 }
 
 const reducer=(state=initialState ,action)=>{
@@ -25,9 +26,27 @@ const reducer=(state=initialState ,action)=>{
         case ORDER_PIZZA:
             return{
                 ...state,
-                pizzaBase:pizzaBase-1
+                pizzaBase:state.pizzaBase-1
             }
         default:
             return state;
     }
 }
+
+// STORE
+// 1- Store needs to hold applocation state
+
+const store = createStore(reducer)
+
+// 2- it exposes a method called getState which gives your application access to the current state in the store.
+console.log('Initial State', store.getState());
+
+// 3- Registers listeners via subscribe(listener)
+store.subscribe(()=>console.log('Updated State',store.getState()))
+
+// 4- Allows state to be update via dispatch(action)
+
+// 執行orderPiazza()得到一個有type的action物件，store.dispatch將這個action發送給store，store收到action後會調用reducer，並且把state、action傳入(即執行reducer)
+// reducer用type判斷並計算返回新的state
+// store更新reducer返回的新state，最後觸發subscribe註冊的監聽器
+store.dispatch(orderPiazza())
